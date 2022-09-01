ae5c608a49dc81338a3d66c571f0610e90cdeccc
Baunie <32245941+Baunie@users.noreply.github.com>
2022-09-02 01:40:32 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+description 'QBR-Skillbar'
+version '1.0.0'
+
+ui_page "html/index.html"
+
+client_script 'client/main.lua'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/crock.ttf'
+}
+
+exports {
+    'GetSkillbarObject'
+}
+
+dependencies {
+    'qbr-core'
+}
+
+lua54 'yes'
